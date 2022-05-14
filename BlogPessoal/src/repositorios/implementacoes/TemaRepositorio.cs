3adaf92ba35adc6c6370cdaa3bceaaf1b7267352
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-05-14 19:32:50 -0300

    [Task: REFATORING code]

---
+++ BlogPessoal/src/repositorios/implementacoes/TemaRepositorio.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPessoal.src.data;
+using BlogPessoal.src.dtos;
+using BlogPessoal.src.modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPessoal.src.repositorios.implementacoes
+{
+    public class TemaRepositorio : ITema
+    {
+        #region Atributos
+
+        private readonly BlogPessoalContexto _contexto;
+
+        #endregion Atributos
+
+
+        #region Construtores
+
+        public TemaRepositorio(BlogPessoalContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        #endregion Construtores
+
+
+        #region Métodos
+
+        public async Task<List<TemaModelo>> PegarTodosTemasAsync()
+        {
+            return await _contexto.Temas.ToListAsync();
+        }
+
+        public async Task<TemaModelo> PegarTemaPeloIdAsync(int id)
+        {
+            return await _contexto.Temas.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<List<TemaModelo>> PegarTemasPelaDescricaoAsync(string descricao)
+        {
+            return await _contexto.Temas
+                            .Where(u => u.Descricao.Contains(descricao))
+                            .ToListAsync();
+        }
+
+        public async Task NovoTemaAsync(NovoTemaDTO tema)
+        {
+            await _contexto.Temas.AddAsync(new TemaModelo
+            {
+                Descricao = tema.Descricao
+            });
+
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task AtualizarTemaAsync(AtualizarTemaDTO tema)
+        {
+            var temaExistente = await PegarTemaPeloIdAsync(tema.Id);
+            temaExistente.Descricao = tema.Descricao;
+            _contexto.Temas.Update(temaExistente);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task DeletarTemaAsync(int id)
+        {
+            _contexto.Temas.Remove(await PegarTemaPeloIdAsync(id));
+            await _contexto.SaveChangesAsync();
+        }
+
+        #endregion Métodos
+    }
+}
---
+++ BlogPessoal/src/repositorios/implementacoes/UsuarioRepositorio.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogPessoal.src.data;
+using BlogPessoal.src.dtos;
+using BlogPessoal.src.modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPessoal.src.repositorios.implementacoes
+{
+    public class UsuarioRepositorio : IUsuario
+    {
+        #region Atributos
+
+        private readonly BlogPessoalContexto _contexto;
+
+        #endregion Atributos
+
+        #region Construtores
+
+        public UsuarioRepositorio(BlogPessoalContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        #endregion Construtores
+
+        #region Métodos
+
+        public async Task<UsuarioModelo> PegarUsuarioPeloIdAsync(int id)
+        {
+            return await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<List<UsuarioModelo>> PegarUsuariosPeloNomeAsync(string nome)
+        {
+            return await _contexto.Usuarios
+                        .Where(u => u.Nome.Contains(nome))
+                        .ToListAsync();
+        }
+
+        public async Task<UsuarioModelo> PegarUsuarioPeloEmailAsync(string email)
+        {
+            return await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task NovoUsuarioAsync(NovoUsuarioDTO usuario)
+        {
+            await _contexto.Usuarios.AddAsync(new UsuarioModelo
+            {
+                Email = usuario.Email,
+                Nome = usuario.Nome,
+                Senha = usuario.Senha,
+                Foto = usuario.Foto,
+                Tipo = usuario.Tipo
+            });
+
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task AtualizarUsuarioAsync(AtualizarUsuarioDTO usuario)
+        {
+            var usuarioExistente = await PegarUsuarioPeloIdAsync(usuario.Id);
+            usuarioExistente.Nome = usuario.Nome;
+            usuarioExistente.Senha = usuario.Senha;
+            usuarioExistente.Foto = usuario.Foto;
+            _contexto.Usuarios.Update(usuarioExistente);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task DeletarUsuarioAsync(int id)
+        {
+            _contexto.Usuarios.Remove(await PegarUsuarioPeloIdAsync(id));
+            await _contexto.SaveChangesAsync();
+        }
+
+        #endregion Métodos
+    }
+}
